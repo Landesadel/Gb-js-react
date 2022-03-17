0c9d06fe5ddb700994dec050b0e2eed26d2bd5a4
Landesadel <iliillimusicforever@gmail.com>
2022-03-17 19:02:25 +0300

    Небольшие доработки кода
    Доделан ответ бота, убран лишний код.

--- homework/src/App.js
+++ homework/src/App.js
@@ -1,59 +1,59 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import MessageItem from './components/MessageItem';
 import MessageForm from './components/MessageForm';
 import './styles/App.css';
-import { toBePartiallyChecked } from '@testing-library/jest-dom/dist/matchers';
+
 
 
 function App() {
-  const [messages, setMessages] = useState([]);
-  const messagesList = [
-    { id: 1, author: 'some author1', text: 'some text1' },
-    { id: 2, author: 'some author2', text: 'some text2' },
-    { id: 3, author: 'some author3', text: 'some text3' },
-  ];
+  const [messagesList, setMessagesList] = useState([]);
+  // const messagesListArr = [
+  //   { id: 1, author: 'some author1', text: 'some text1' },
+  //   { id: 2, author: 'some author2', text: 'some text2' },
+  //   { id: 3, author: 'some author3', text: 'some text3' },
+  // ];
 
-  function addMessages() {
-    setMessages(messagesList)
-  }
-  function rmvMessages() {
-    setMessages([])
+  // function addMessages() {
+  //   setMessagesList(messagesListArr)
+  // }
+  function rmvMessages() { //rкнопка очистки массива
+    setMessagesList([])
   }
 
-  const createMessage = (newPost) => {
-    setMessages([...messages, newPost])
+  const createMessage = (newPost) => { // принимает объект с компонента MessageForm, и добавляет в массив, обнавляя значение стейта
+    setMessagesList([...messagesList, newPost])
 
   }
   
   useEffect(() => {
-    const botText = 'test'
-    const lastMess = messages[messages.length - 1]
+    const botText = { author: 'Bot', text: 'HEllo from BOt!V_o_O_V' };
+    const botStr = JSON.stringify(botText);
+    const lastMess = JSON.stringify(messagesList[messagesList.length - 1]);
     let timerId = null
 
-    if (lastMess !== botText) {
+    if (lastMess !== botStr) {
       timerId = setTimeout(() => {
-        setMessages([...messages, botText])
+        setMessagesList([...messagesList, botText])
       } , 700)  
     }
     return () => {
       clearInterval(timerId)
     }
      
-    }, [messages])
+    }, [messagesList])
 
   return (
     <div className='App'>
       <div className='rend-box'>
         <MessageForm create={createMessage}/>
-      <div className='message-box'>
-         {messages.map((message, index) => 
+      <div className='message-box'> 
+         {messagesList.map((message, index) => //перебираем массив мепом , и отображаем его через компонент MessageItem (верстка) 
            <MessageItem number={index + 1} post={message} key={message.id} />
           )} 
                 
       </div>
-        <div className='btn-box'>
-          
-          <button className='btn-mes' onClick={addMessages}>Add messages</button>
+        <div className='btn-box'>      
+          {/* <button className='btn-mes' onClick={addMessages}>Add messages</button> */}
           <button className='btn-mes' onClick={rmvMessages}>Remove</button>
       </div>
     </div>
--- homework/src/components/MessageForm.jsx
+++ homework/src/components/MessageForm.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import '../styles/MessageForm.css';
 
 const  MessageForm = ({create}) => {
