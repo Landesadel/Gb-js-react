1d60e5e3f9f2bf1b66f7da07398f5c64e6a5446f
Landesadel <iliillimusicforever@gmail.com>
2022-04-01 19:32:15 +0300

    homework-5
    создание редьюсера профиля - в процессе

--- homework/src/pages/Profile.jsx
+++ homework/src/pages/Profile.jsx
@@ -1,7 +1,17 @@
+import { useSelector } from "react-redux";
+
 export const ProfilePage = () => {
+   const {firstName, lastName, visibleProfile} = useSelector(state => state)
    return (
       <div>
          <h1>profile</h1>
+         {visibleProfile && (
+            <>
+               <h3>firstName: <span>{firstName}</span></h3>
+               <h3>lastName: <span>{lastName}</span></h3>
+            </>
+         )}
+         
       </div>
    );
 };
---
+++ homework/src/store/profile/reducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+   visibleProfile: true,
+   firstName: 'firstName',
+   lastName: 'lastName',
+};
+
+export const profileReducer = (state = initialState, action) => {
+   switch (action.type) {
+      default:
+         return state;
+   }
+};
